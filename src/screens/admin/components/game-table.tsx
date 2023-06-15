cf22f151a5d9aafec59a77fc591a700cfb1c7f72
Dzmitry-Klokau <71276547+Dzmitry-Klokau@users.noreply.github.com>
2023-06-15 22:24:04 +0300

    game admin page

--- src/screens/admin/components/game-table.tsx
+++ src/screens/admin/components/game-table.tsx
@@ -1,8 +1,17 @@
-import { Typography } from "@mui/material";
+import { Box, CardActionArea, Paper, Typography } from "@mui/material";
 import { isEmpty } from "lodash";
 import { memo, useMemo, useState } from "react";
-
-import { ModalWrapper, Table } from "../../../shared/components";
+import {
+  Link,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Card,
+  Grid,
+} from "@mui/material";
 
 type Props = {
   visible: boolean;
@@ -10,54 +19,116 @@ type Props = {
 };
 
 export const GameTable = memo(({ data, visible }: Props) => {
-  const [modalData, setModalData] = useState<GameType>();
-
-  const table = useMemo(
-    () => (
-      <Table
-        head={["Title", ""]}
-        data={data}
-        fields={[
-          { field: "title" },
-          { field: "Details", onClick: (row) => setModalData(row) },
-        ]}
-        keyField="uid"
-      />
-    ),
-    [data]
-  );
+  const [selected, setSelected] = useState<GameType>();
 
   if (isEmpty(data) || !visible) return null;
 
   return (
     <>
-      {table}
-      <ModalWrapper
-        open={!!modalData}
-        onClose={() => {
-          setModalData(undefined);
-        }}
-      >
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          {`Uid: ${modalData?.uid}`}
-        </Typography>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          {`title: ${modalData?.title}`}
-        </Typography>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          {`Description: ${modalData?.desc}`}
-        </Typography>
-        {modalData?.cards.map((c, index) => (
-          <Typography
-            key={index}
-            id="modal-modal-title"
-            variant="h6"
-            component="h2"
-          >
-            {`${c.primary}-${c.secondary}-${c.optional}`}
+      <TableContainer component={Paper} sx={{ ph: 1 }}>
+        <Table sx={{ minWidth: 350 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => {
+              const isSelected = row.uid === selected?.uid;
+              return (
+                <>
+                  <TableRow
+                    key={row.uid}
+                    role="button"
+                    onClick={() => {
+                      setSelected(row);
+                    }}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                      cursor: "pointer",
+                      backgroundColor: (theme) =>
+                        isSelected ? theme.palette.grey[300] : undefined,
+                    }}
+                  >
+                    <TableCell>{row.title}</TableCell>
+                    <TableCell>{row.desc}</TableCell>
+                  </TableRow>
+                </>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {selected && (
+        <TableContainer component={Paper} sx={{ ph: 1, mt: 6 }}>
+          <Typography variant="h5" component="h5" sx={{ p: 2 }}>
+            Selected item:
           </Typography>
-        ))}
-      </ModalWrapper>
+          <Table sx={{ minWidth: 350 }} aria-label="simple table">
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Typography>UID</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{selected.uid}</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography>Title</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{selected.title}</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <Typography>Description</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{selected.desc}</Typography>
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell colSpan={2}>
+                  <Grid container spacing={2}>
+                    {selected.cards.map((card, index) => (
+                      <Grid
+                        key={`${selected.uid}-${index}`}
+                        item
+                        md={4}
+                        xs={12}
+                        sx={{ alignContent: "center" }}
+                      >
+                        <Card variant="outlined">
+                          <CardActionArea
+                            onClick={() => {
+                              //
+                            }}
+                            sx={{ p: 2 }}
+                          >
+                            <Typography>
+                              <strong>P:</strong> {card.primary}
+                            </Typography>
+                            <Typography>
+                              <strong>S:</strong> {card.secondary}
+                            </Typography>
+                            <Typography>
+                              <strong>O:</strong> {card.optional}
+                            </Typography>
+                          </CardActionArea>
+                        </Card>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
     </>
   );
 });
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,2 @@
 export * from "./copyright";
-export * from "./modal-wrapper";
 export * from "./table";
