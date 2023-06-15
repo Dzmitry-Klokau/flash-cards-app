7c9a39928e2f579c0bedca86359df4cbae5fa6b3
Dzmitry-Klokau <71276547+Dzmitry-Klokau@users.noreply.github.com>
2023-06-15 20:30:37 +0300

    Update mock-games.ts

--- src/mocks/mock-games.ts
+++ src/mocks/mock-games.ts
@@ -1,6 +1,6 @@
 export const mockGameList: Array<GameType> = [
   {
-    desc: "Descit",
+    desc: "Общие фразы в литовском языке",
     title: "Общие фразы",
     uid: "i7vUjv9kYnrdhnnepDpy",
     cards: [
@@ -9,12 +9,87 @@ export const mockGameList: Array<GameType> = [
     ],
   },
   {
-    desc: "Descit",
-    title: "Общие фразы 2",
-    uid: "23edscsd2e23cdcsd",
+    desc: "Полный список общих фраз",
+    title: "Общие фразы (полный)",
+    uid: "d32scd34cds43ed4332e",
     cards: [
-      { optional: "Ня", primary: "Нет", secondary: "Ne" },
+      { primary: "Нет", secondary: "Ne", optional: "Ня" },
       { primary: "Да", secondary: "Taip", optional: "Тэйп" },
+      {
+        primary: "Здравствуйте!",
+        secondary: "Sveiki!",
+        optional: "Свейкѝ",
+      },
+      {
+        primary: "Привет!",
+        secondary: "Labas!",
+        optional: "Ла̀бас",
+      },
+      {
+        primary: "Доброе утро!",
+        secondary: "Labas rytas!",
+        optional: "Ла̀бас рѝтас",
+      },
+      {
+        primary: "Добрый день!",
+        secondary: "Laba diena!",
+        optional: "Лаба̀ дена̀",
+      },
+      {
+        primary: "Добрый вечер!",
+        secondary: "Labas vakaras!",
+        optional: "Ла̀бас ва̀карас",
+      },
+      {
+        primary: "Спокойной ночи!",
+        secondary: "Labanakt!",
+        optional: "Лаба̀накт!",
+      },
+      {
+        primary: "Доброй ночи!",
+        secondary: "Geros nakties!",
+        optional: "Гяро̀с нактѐс",
+      },
+      {
+        primary: "Всего хорошего!",
+        secondary: "Viso gero!",
+        optional: "Вѝсо гя̀ро",
+      },
+      {
+        primary: "До свидания!",
+        secondary: "Iki pasimatymo!",
+        optional: "Икѝ пасима̀тимо!",
+      },
+      {
+        primary: "До завтра!",
+        secondary: "Iki rytojaus!",
+        optional: "Икѝ рито̀яус!",
+      },
+      {
+        primary: "Счастливо!",
+        secondary: "Sėkmės!",
+        optional: "Секмѐс!",
+      },
+      {
+        primary: "Пока",
+        secondary: "Iki",
+        optional: "Икѝ",
+      },
+      {
+        primary: "Спасибо",
+        secondary: "Ačiū",
+        optional: "А̀чю",
+      },
+      {
+        primary: "Пожалуйста",
+        secondary: "Prašau",
+        optional: "Праша̀у",
+      },
+      {
+        primary: "Извините",
+        secondary: "Atsiprašau",
+        optional: "Атсипраша̀у",
+      },
     ],
   },
 ];
